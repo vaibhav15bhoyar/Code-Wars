9bed0dc9ae0c519b974dcd40345b6e9876122457
Nihal Bambale <nihalbambale2000@gmail.com>
2024-06-27 23:14:59 +0530

    Sum of array singles commited

---
+++ CPP/7-kyu/Sum of array singles.cpp
@@ -0,0 +1,36 @@
+/*
+    In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+More examples in the test cases.
+
+Good luck!
+
+*/
+
+
+#include <iostream>
+#include<vector>
+#include <utility>  
+#include<map>
+using namespace std;
+int repeats(std::vector<int> v)
+{
+    map<int, int> mp;
+    for (int i = 0; i < v.size(); i++)
+    {
+        mp[v[i]]++;
+    }
+
+    int sum = 0;
+    for (pair p : mp)
+    {
+        if (p.second == 1)
+        {
+            sum = sum + p.first;
+        }
+    }
+
+    return sum;
+}
